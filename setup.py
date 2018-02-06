2cbe2cb268acd0b7dceba4d8748898583fa7cf0d
Matthew Treinish <mtreinish@kortar.org>
2018-02-06 18:20:14 -0500

    Fix copyright headers

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
-# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+# Copyright 2018 Matthew Treinish
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# This file is part of pyopnsense
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+# pyopnsense is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# pyopnsense is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyopnsense. If not, see <http://www.gnu.org/licenses/>.
 
-# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
 import setuptools
 
 # In python < 2.7.4, a lazy loading of package `pbr` will break
