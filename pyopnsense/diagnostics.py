074cb6dbb9e20856113361539694725b060ac11e
Matthew Treinish <mtreinish@kortar.org>
2018-02-13 15:44:46 -0500

    Flush out diagnostics module
    This commit adds the missing client classes and methods for the missing
    methods off the /diagnostics endpoint. This has coverage for all the api
    calls I could find.

--- pyopnsense/diagnostics.py
+++ pyopnsense/diagnostics.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with pyopnsense. If not, see <http://www.gnu.org/licenses/>.
 
+from six.moves import urllib
+
 from pyopnsense import client
 
 
@@ -22,3 +24,51 @@ class NetFlowClient(client.OPNClient):
 
     def status(self):
         return self._get('diagnostics/netflow/status')
+
+
+class InterfaceClient(client.OPNClient):
+
+    def get_ndp(self):
+        return self._get('diagnostics/interface/getNdp')
+
+    def get_arp(self):
+        return self._get('diagnostics/interface/getArp')
+
+
+class NetworkInsightClient(client.OPNClient):
+
+    def get_interfaces(self):
+        return self._get('diagnostics/networkinsight/getinterfaces')
+
+    def get_services(self):
+        return self._get('diagnostics/networkinsight/getservices')
+
+    def get_protocols(self):
+        return self._get('diagnostics/networkinsight/getprotocols')
+
+    def get_timeserie(self):
+        return self._get('diagnostics/networkinsight/timeserie')
+
+
+class SystemHealthClient(client.OPNClient):
+
+    def get_health_list(self):
+        return self._get('diagnostics/systemhealth/getRRDlist')
+
+    def get_health_data(self, metric, start=0, stop=0, maxitems=1024,
+                        inverse=False, details=False):
+        url = ['diagnostics/systemhealth/getSystemHealth']
+        url.append(urllib.parse.quote(metric))
+        url.append(start)
+        url.append(stop)
+        url.append(maxitems)
+        if inverse:
+            url.append('true')
+        else:
+            url.append('false')
+        if details:
+            url.append('true')
+        else:
+            url.append('false')
+
+        return self._get('/'.join(url))
