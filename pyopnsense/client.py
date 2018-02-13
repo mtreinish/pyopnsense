dd1f46128010345c5630ab5a0b4da1358c94bd7d
Matthew Treinish <mtreinish@kortar.org>
2018-02-13 11:32:25 -0500

    Expand base client to support most HTTP methods
    This commit expands the base client to support most HTTP methods. This
    should cover all the commonly used API use cases.

--- pyopnsense/client.py
+++ pyopnsense/client.py
@@ -22,6 +22,9 @@ import requests
 
 from pyopnsense import exceptions
 
+# All the successful HTTP status codes from RFC 7231 & 4918
+HTTP_SUCCESS = (200, 201, 202, 203, 204, 205, 206, 207)
+
 
 class OPNClient(object):
 
@@ -31,12 +34,45 @@ class OPNClient(object):
         self.base_url = base_url
         self.verify_cert = verify_cert
 
-    def _get(self, url):
-        req_url = self.base_url + '/' + url
-        response = requests.get(req_url, verify=self.verify_cert,
-                                auth=(self.api_key, self.api_secret))
-        if response.status_code == 200:
+    def _process_response(self, response):
+        if response.status_code in HTTP_SUCCESS:
             return json.loads(response.text)
         else:
             raise exceptions.APIException(status_code=response.status_code,
                                           resp_body=response.text)
+
+    def _get(self, url):
+        req_url = self.base_url + '/' + url
+        response = requests.get(req_url, verify=self.verify_cert,
+                                auth=(self.api_key, self.api_secret))
+        return self._process_response(response)
+
+    def _head(self, url):
+        req_url = self.base_url + '/' + url
+        response = requests.head(req_url, verify=self.verify_cert,
+                                 auth=(self.api_key, self.api_secret))
+        return self._process_response(response)
+
+    def _post(self, url, body):
+        req_url = self.base_url + '/' + url
+        response = requests.post(req_url, data=body, verify=self.verify_cert,
+                                 auth=(self.api_key, self.api_secret))
+        return self._process_response(response)
+
+    def _put(self, url, body):
+        req_url = self.base_url + '/' + url
+        response = requests.put(req_url, data=body, verify=self.verify_cert,
+                                auth=(self.api_key, self.api_secret))
+        return self._process_response(response)
+
+    def _delete(self, url):
+        req_url = self.base_url + '/' + url
+        response = requests.delete(req_url, verify=self.verify_cert,
+                                   auth=(self.api_key, self.api_secret))
+        return self._process_response(response)
+
+    def _patch(self, url, body):
+        req_url = self.base_url + '/' + url
+        response = requests.patch(req_url, data=body, verify=self.verify_cert,
+                                  auth=(self.api_key, self.api_secret))
+        return self._process_response(response)
