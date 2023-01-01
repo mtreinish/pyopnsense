6419d2cac2adabfb4869d86e0f4879c8eb0b1675
Angvoreeth <angvoreeth@inyx.be>
2023-01-01 18:27:07 +0100

    Default timeout value changed so as to fix the OPNSense integration of HomeAssistant

--- pyopnsense/client.py
+++ pyopnsense/client.py
@@ -22,7 +22,7 @@ import requests
 
 from pyopnsense import exceptions
 
-DEFAULT_TIMEOUT = 5
+DEFAULT_TIMEOUT = 20
 
 # All the successful HTTP status codes from RFC 7231 & 4918
 HTTP_SUCCESS = (200, 201, 202, 203, 204, 205, 206, 207)
